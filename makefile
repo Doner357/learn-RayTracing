8aaee62a20847c0baabe4fa7809096eac2e01d87
Doner <don468sam@gmail.com>
2024-07-20 22:34:28 +0800

    Edit makefile and program output.

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 ####################################################
-# compile
+# Compile
 ####################################################
 # Target file
 TARGET := Test
@@ -11,9 +11,9 @@ CXX := g++
 CXXVERSION := c++20
 
 # Directories
-TARGET_DIR  := bin
+TARGET_DIR  := build/bin
 SRC_DIR     := src
-BUILD_DIR   := build
+BUILD_DIR   := objs
 INCLUDE_DIR := include
 
 # Compiler flags
@@ -35,10 +35,9 @@ OBJS += $(patsubst $(SRC_DIR)/%.c, $(BUILD_DIR)/%.o, $(C_SRCS))
 DEPS := $(OBJS:.o=.d)
 
 
-# Deal the difference of the operating system
+# Deal with the difference of the operating system
 ifdef OS
 	TARGET       := $(TARGET).exe
-	PROJECT_NAME := $(PROJECT_NAME).exe
 	RM            = del /Q
 	FixPath       = $(subst /,\, $(1))
 	MKDIR         = mkdir
@@ -55,30 +54,30 @@ BUILDTARGET := $(TARGET_DIR)/$(TARGET)
 
 # Default target
 all: $(BUILDTARGET)
-	@echo Building finished!
 
 # Rule to link executable
 $(BUILDTARGET): $(OBJS) | $(TARGET_DIR)
-	@echo Start linking...
+	@echo Starting linking...
 	@$(CXX) $(OBJS) -o $@ $(LDFLAGS)
+	@echo Building finished!
 
 # Rule to compile source files into object files
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp | $(BUILD_DIR)
-	@echo Start compiling cpp souce files to object file: $<
+	@echo Compiling C++ source file to object file: $<
 	@$(CXX) $(CXXFLAGS) -c $< -o $@
 
 # Rule to compile C source files into object files
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.c | $(BUILD_DIR)
-	@echo Start compiling c souce files to object file: $<
+	@echo Compiling C source file to object file: $<
 	@$(CXX) $(CXXFLAGS) -c $< -o $@
 
 # Ensure the build directory exists
 $(TARGET_DIR):
-	@$(MKDIR) $(TARGET_DIR)
+	@$(MKDIR) $(call FixPath, $(TARGET_DIR))
 
 # Ensure the build directory exists
 $(BUILD_DIR):
-	@$(MKDIR) $(BUILD_DIR)
+	@$(MKDIR) $(call FixPath, $(BUILD_DIR))
 
 # Include dependency files if they exist
 -include $(DEPS)
@@ -87,7 +86,7 @@ $(BUILD_DIR):
 ####################################################
 # Clean
 ####################################################
-# Files need to be Clean
+# Files to be cleaned
 RM_FILES := $(BUILDTARGET)       \
 			$(OBJS)              \
 			$(DEPS)              \
@@ -96,22 +95,21 @@ RM_FILES := $(BUILDTARGET)       \
 
 # Clean up build directory
 clean:
-	@echo Start deleting created files...
+	@echo Starting to delete created files...
 	@$(RM) $(call FixPath, $(RM_FILES))
 	@echo Done!
-
 
 ####################################################
 # Run
 ####################################################
-# Files need to be execute
+# File to be executed
 EXECUTE := ./$(TARGET)
 
 # Run method
 run: $(BUILDTARGET)
 	@echo -------------------------- Start Running --------------------------
 	@cd $(TARGET_DIR) && $(call FixPath, $(EXECUTE))
-	@echo --------------------------  End  Running --------------------------
+	@echo -------------------------- End Running --------------------------
 
 # Phony targets
-.PHONY: all clean
+.PHONY: all clean run
--- src/main.cpp
+++ src/main.cpp
@@ -13,51 +13,58 @@ int main() {
 
     // Store image color data
     std::vector<uint8_t> image_data;
-    // .ppm file output stream
-    std::ofstream ppm_image;
-    ppm_image.open("ppm_image.ppm");
 
-    int image_width  = 256;
-    int image_height = 256;
-    int image_comp   = 3;    // R, G, B
+    int32_t image_width  = 256;
+    int32_t image_height = 256;
+    int32_t image_comp   = 3;    // R, G, B
 
     // -- Render --
 
-    // For .ppm file
-    ppm_image << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; j++) {
+    for (int32_t j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) {
+        for (int32_t i = 0; i < image_width; i++) {
             double r = static_cast<double>(i) / (image_width - 1);
             double g = static_cast<double>(j) / (image_width - 1);
             double b = 0.0;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            ppm_image << ir << ' ' << ig << ' ' << ib << '\n';
+            int32_t ir = static_cast<int32_t>(255.999 * r);
+            int32_t ig = static_cast<int32_t>(255.999 * g);
+            int32_t ib = static_cast<int32_t>(255.999 * b);
 
             image_data.push_back(ir);
             image_data.push_back(ig);
             image_data.push_back(ib);
         }
     }
-
     std::clog << "\rDone.                       \n";
 
+
+    // Output .ppm file
+    std::ofstream ppm_image;
+    ppm_image.open("ppm_image.ppm");
+    // .ppm Header
+    ppm_image << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    // .ppm color data
+    std::cout << image_data.size() / 3 << ' ' << image_width * image_height << std::endl;
+    for (uint32_t i = 0; i < image_data.size(); i += 3) {
+        ppm_image << static_cast<int32_t>(image_data[i])     << ' ' <<  // R
+                     static_cast<int32_t>(image_data[i + 1]) << ' ' <<  // G
+                     static_cast<int32_t>(image_data[i + 2]) << '\n';   // B
+    }
     // End output .ppm file
     ppm_image.close();
 
+
     // Test stbi_image_write
+    // Output png file
     stbi_write_png("png_image.png", image_width, image_height, image_comp, image_data.data(), image_width * image_comp);
+
 
     // Test stbi_image
     int load_image_width  = 0,
         load_image_height = 0,
         load_image_comp   = 0;
-
+    // Load image output previously
     unsigned char* load_image_data = stbi_load("png_image.png", &load_image_width, &load_image_height, &load_image_comp, 0);
     if (load_image_data) {
         std::clog << "Load successfully!" << '\n' <<
@@ -68,7 +75,6 @@ int main() {
     else {
         std::clog << "Failed to load image!" << std::endl;
     }
-
     stbi_image_free(load_image_data);
 
     return 0;
