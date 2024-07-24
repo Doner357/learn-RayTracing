4562cfd3fb3676ac876f32ae25da114c82c0dad4
Doner <don468sam@gmail.com>
2024-07-24 16:52:17 +0800

    Adding a shere into the scene.

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@
 
 // Return the ray attributes as color
 color RayColor(const ray& r);
+// Check whether a ray will hit a sphere
+bool HitSphere(const point3& center, double radius, const ray& r);
 
 int main() {
 
@@ -49,7 +51,7 @@ int main() {
     vec3 pixel_delta_u = viewport_u / image_width;
     vec3 pixel_delta_v = viewport_v / image_height;
 
-    // Calculate the location of the upper left pixel
+    // Calculate the location of the upper left pixel.
     point3 viewport_upper_left = camera_center -
                                  vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
     point3 pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
@@ -82,10 +84,26 @@ int main() {
 }
 
 color RayColor(const ray& r) {
+    // Check if the ray will hit the hard-code sphere
+    if (HitSphere(point3(0.0, 0.0, 1.0), 0.5, r)) {
+        return color(1.0, 0.0, 0.0);    // red
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     // Map values ​​[-1.0, 1.0] to [0.0, 1.0]
     double a = 0.5 * (unit_direction.y() + 1.0);
     // Do linear interpolation " blendedValue = (1−a) * startValue + a * endValue "
     // to interpolate the blue and white color according to the y scale.
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
+bool HitSphere(const point3& center, double radius, const ray& r) {
+    // Solve quadratic equation for sphere intersection.
+    // Then use b * b - 4ac to determine if the ray will intersect the sphere
+    vec3 oc = center - r.origin();
+    double a = dot(r.direction(), r.direction());
+    double b = -2.0 * dot(r.direction(), oc);
+    double c = dot(oc, oc) - radius * radius;
+    double discriminant = b * b - 4*a*c;
+    return (discriminant >= 0);
 }
