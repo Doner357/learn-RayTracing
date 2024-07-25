10d570557da8760bd7081aca87830f80e765d106
Doner <don468sam@gmail.com>
2024-07-25 16:51:04 +0800

    Add code to determine whether the hitting ray is from the front or back.

--- src/headers/hittable.hpp
+++ src/headers/hittable.hpp
@@ -8,12 +8,22 @@
 #define HITTABLE_HPP
 
 #include "ray.hpp"
+#include "vec3.hpp"
 
 class hit_record {
     public:
         point3 p;
         vec3   normal;
         double t;
+        bool front_face;    // Store the hit is from front or back
+
+        void set_face_normal(const ray& r, const vec3& outward_normal) {
+            // Sets the hit record normal vector
+            // NOTE: the parameter `outward_normal` is assumed to have unit length.
+
+            front_face = dot(r.direction(), outward_normal) < 0;
+            normal = front_face ? outward_normal : -outward_normal;
+        }
 };
 
 class hittable {
