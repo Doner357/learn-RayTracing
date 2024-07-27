52a5f2f5694767ef42fd1f58e44314630012fbc3
Doner <don468sam@gmail.com>
2024-07-27 22:02:43 +0800

    Render fuzzed metals.

--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,8 @@ int main() {
 
     std::shared_ptr<lambertian> material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
     std::shared_ptr<lambertian> material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    std::shared_ptr<metal>      material_left   = std::make_shared<metal>(color(0.8, 0.8, 0.8));
-    std::shared_ptr<metal>      material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2));
+    std::shared_ptr<metal>      material_left   = std::make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    std::shared_ptr<metal>      material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
@@ -39,7 +39,7 @@ int main() {
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
 
-    cam.render(world, "shiny_metal");
+    cam.render(world, "fuzzed_metal");
 
     
     return 0;
