ab7ef2fdf0ef77c6aa0d324d0a5b5069bc53a435
Doner <don468sam@gmail.com>
2024-07-29 15:21:46 +0800

    Render a distanct view with zoom far.

--- src/headers/camera.hpp
+++ src/headers/camera.hpp
@@ -25,7 +25,10 @@ class Camera {
         uint32_t samples_per_pixel = 10;   // Count of random samples for each pixel
         int32_t  max_depth         = 10;   // Maximum number of ray bounces into scene
 
-        double vfov = 90.0; // Vertical view angle (field of view)
+        double vfov     = 90.0; // Vertical view angle (field of view)
+        point3 lookfrom = point3( 0.0,  0.0,  0.0); // Point camera is looking from
+        point3 lookat   = point3( 0.0,  0.0, -1.0); // Point camera is looking at
+        vec3   vup      = vec3(0.0, 1.0, 0.0);      // Camera-relative "up" direction 
 
 
         void render(const hittable& world, std::string image_name) {
@@ -67,6 +70,7 @@ class Camera {
         point3   pixel00_loc;          // Location of pixel 0, 0
         vec3     pixel_delta_u;        // Offset to pixel to the right
         vec3     pixel_delta_v;        // Offset to pixel below
+        vec3     u, v, w;              // Camera frame basis vectors
 
 
         void initialize() {
@@ -76,25 +80,30 @@ class Camera {
 
             pixel_samples_scale = 1.0 / samples_per_pixel;
 
-            center = point3(0, 0, 0);
+            center = lookfrom;
             
-            double focal_length    = 1.0;
+            // Determine viewport dimensions.
+            double focal_length    = (lookfrom - lookat).length();
             double theta           = degrees_to_radians(vfov);
             double h               = std::tan(theta / 2);
             double viewport_height = 2 * h * focal_length;
             double viewport_width  = viewport_height * (static_cast<double>(image_width) / image_height);
 
+            // Calculate the u, v, w unit basis vectors for the camera coordinate frame
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+
             // Calculate the vectors across the horizontal and down the vertical viewport edges.
-            vec3 viewport_u = vec3(viewport_width, 0, 0);
-            vec3 viewport_v = vec3(0, -viewport_height, 0);
+            vec3 viewport_u = viewport_width * u;    // Vector across viewport horizontal edge
+            vec3 viewport_v = viewport_height * -v;  // Vector down viewport vertical edge
 
             // Calculate the horizontal and vertical delta vectors from pixel to pixel.
             pixel_delta_u = viewport_u / image_width;
             pixel_delta_v = viewport_v / image_height;
 
             // Calculate the location of the upper left pixel.
-            point3 viewport_upper_left =
-                center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+            point3 viewport_upper_left = center - (focal_length * w) - viewport_u / 2.0 - viewport_v / 2.0;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
         }
 
