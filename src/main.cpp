af5cef5c3c584e6f06d786e6a492f6e6f31ce008
Doner <don468sam@gmail.com>
2024-07-30 13:32:05 +0800

    Render Fianl Scene.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstdint>
 #include <memory>
+#include <chrono>
 
 #include <stb/stb_image.h>
 #include <stb/stb_image_write.h>
@@ -20,38 +21,77 @@ int main() {
     // -- World --
     hittable_list world;
 
-    std::shared_ptr<lambertian> material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    std::shared_ptr<lambertian> material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    std::shared_ptr<dielectric> material_left   = std::make_shared<dielectric>(1.50);        // Outer glass
-    std::shared_ptr<dielectric> material_bubble = std::make_shared<dielectric>(1.00 / 1.50); // inner air
-    std::shared_ptr<metal>      material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto ground_material = std::make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(std::make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-    world.add(std::make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(std::make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
-    world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
-    world.add(std::make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                std::shared_ptr<material> sphere_material;
 
-    // -- Canmera --
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = std::make_shared<lambertian>(albedo);
+                    world.add(std::make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = std::make_shared<metal>(albedo, fuzz);
+                    world.add(std::make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = std::make_shared<dielectric>(1.5);
+                    world.add(std::make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = std::make_shared<dielectric>(1.5);
+    world.add(std::make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = std::make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(std::make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = std::make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(std::make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
     Camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 400;
-    cam.samples_per_pixel = 100;
+    cam.image_width       = 1920;
+    cam.samples_per_pixel = 500;
     cam.max_depth         = 50;
 
-    cam.vfov     = 20.0;    // FOV is 20 degrees
-    cam.lookfrom = point3(-2.0, 2.0, 1);
-    cam.lookat   = point3(0.0, 0.0, -1.0);
-    cam.vup      = vec3(0.0, 1.0, 0.0);
+    cam.vfov     = 20;
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
 
-    // Defocus
-    cam.defocus_angle = 10.0;
-    cam.focus_dist    = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist    = 10.0;
 
-    cam.render(world, "depth-of-field");
-
+    std::chrono::time_point start_time = std::chrono::steady_clock::now();  // Timer
+    cam.render(world, "Ray_Tracing_in_On_Weekend--Final_Render_1920x");
+    std::chrono::time_point end_time = std::chrono::steady_clock::now();
+    std::chrono::duration<double> duration = end_time - start_time;
     
+    std::chrono::duration time = end_time - start_time;
+    std::chrono::hours h = std::chrono::duration_cast<std::chrono::hours>(time);
+    time -= h;
+    std::chrono::minutes m = std::chrono::duration_cast<std::chrono::minutes>(time);
+    time -= m;
+    std::chrono::seconds s = std::chrono::duration_cast<std::chrono::seconds>(time);
+
+    std::ofstream time_record;
+    time_record.open("time.txt");
+    time_record << "Takes: " << h << ' ' << m << ' ' << s << std::endl;
+    time_record.close();
+
     return 0;
 }
