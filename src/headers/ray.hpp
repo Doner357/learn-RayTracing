70f46a1de6855ca815c3fdd2d41e0f444a875090
Doner <don468sam@gmail.com>
2024-08-03 00:32:21 +0800

    Add moving sphere codes.

--- src/headers/ray.hpp
+++ src/headers/ray.hpp
@@ -14,10 +14,16 @@ class ray {
     public:
         ray() {}
 
-        ray(const point3& origin, const vec3& direction) : orig(origin), dir(unit_vector(direction)) {}
+        ray(const point3& origin, const vec3& direction) :
+        orig(origin), dir(unit_vector(direction)), tm(0) {}
+
+        ray(const point3& origin, const vec3& direction, double time) :
+        orig(origin), dir(unit_vector(direction)), tm(time) {}
 
         const point3& origin() const { return orig; }
         const vec3& direction() const { return dir; }
+
+        double time() const { return tm; }
 
         // Function for ray P(t) = origin + t * direction
         point3 at(double t) const {
@@ -27,6 +33,7 @@ class ray {
     private:
         point3 orig;
         vec3 dir;
+        double tm;
 };
 
 
--- src/headers/sphere.hpp
+++ src/headers/sphere.hpp
@@ -17,12 +17,21 @@
 
 class sphere : public hittable {
     public :
+        // Stationary Sphere
         sphere(const point3& center, double radius, std::shared_ptr<material> mat) :
-        center(center), radius(std::max(0.0, radius)), mat(mat) {}
+        center1(center), radius(std::max(0.0, radius)), mat(mat), is_moving(false) {}
+
+        // Moving sphere
+        sphere(const point3& center1, const point3& center2, double radius,
+               std::shared_ptr<material> mat) :
+        center1(center1), radius(std::max(0.0, radius)), mat(mat), is_moving(true) {
+            center_vec = center2 - center1;
+        }
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             // Solve quadratic equation for sphere intersection.
             // Use a simplified solution.
+            point3 center = is_moving ? sphere_center(r.time()) : center1;
             vec3 oc = center - r.origin();
             double a = r.direction().length_squared();
             double h = dot(r.direction(), oc);
@@ -56,9 +65,17 @@ class sphere : public hittable {
 
     
     private:
-        point3 center;
+        point3 center1;
         double radius;
         std::shared_ptr<material> mat;
+        bool is_moving;
+        vec3 center_vec;
+
+        point3 sphere_center(double time) const {
+            // Linearly interpolate from center1 to center2 according to time, where t=0 yields
+            // center1, and t=1 yields center2.
+            return center1 + time * center_vec;
+        }
 };
 
 #endif // SPHERE_HPP
