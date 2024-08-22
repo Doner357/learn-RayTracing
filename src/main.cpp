1c63b5abfb7735d22678b1b8fe261e263302cf79
Doner <don468sam@gmail.com>
2024-08-22 15:26:55 +0800

    Using Monte Carlo to estiamte PI. (Ver.3)

--- src/main.cpp
+++ src/main.cpp
@@ -51,21 +51,29 @@ int main() {
     */
 
     int32_t inside_circle = 0;
-    int32_t runs = 0;
-    std::cout << std::fixed << std::setprecision(12);
-    while(true) {
-        runs++;
-        double x = random_double(-1, 1);
-        double y = random_double(-1, 1);
-        if (x * x + y * y < 1) {
-            inside_circle++;
-        }
-        if (runs % 100000 == 0) {
-            std::cout << "Estimate of Pi = "
-                      << (4.0 * inside_circle) / runs
-                      << '\n';
+    int32_t inside_circle_stratified = 0;
+    int32_t sqrt_N = 1000;
+    for (int32_t i = 0; i < sqrt_N; ++i) {
+        for (int32_t j = 0; j < sqrt_N; j++) {
+            double x = random_double(-1, 1);
+            double y = random_double(-1, 1);
+            if (x * x + y * y < 1){
+                inside_circle++;
+            }
+            x = 2 * ((i + random_double()) / sqrt_N) - 1;
+            y = 2 * ((j + random_double()) / sqrt_N) - 1;
+            if (x * x + y * y < 1) {
+                inside_circle_stratified++;
+            }
         }
     }
+
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout
+        << "Regular    Estimate of Pi = "
+        << (4.0 * inside_circle) / (sqrt_N * sqrt_N) << '\n'
+        << "Stratified Estimate of Pi = "
+        << (4.0 * inside_circle_stratified) / (sqrt_N * sqrt_N) << '\n';
 
     return 0;
 }
